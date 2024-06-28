2000ece94de66fde70b750d5cd73697bb23363d6
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-06-28 21:06:47 +0700

    style: add style,font and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: 'var(--background-color)',
+        container: 'var(--container-color)',
+        stroke: 'var(--stroke-color)',
+        primary: 'var(--primary-color)',
+        title: 'var(--title-color)',
+        text: 'var(--text-color)',
+        subtext: 'var(--text-secondary)',
+      },
+    }
   },
-  plugins: [],
-}
+  plugins: [require('tailwind-scrollbar-hide')],
+};
 
