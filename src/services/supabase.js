ce16f5393c4c4b1da38d4863c661d62526bc3604
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-07-01 23:24:01 +0700

    add: supabase admin auth

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,14 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vfhehgvpfmmkwycnumrz.supabase.co'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const supabaseServiceRole = import.meta.env.VITE_SUPABASE_SERVICE_ROLE
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
+export const adminSupabase = createClient(supabaseUrl, supabaseServiceRole, {
+    auth: {
+        autoRefreshToken: false,
+        persistSession: false
+    }
+})
