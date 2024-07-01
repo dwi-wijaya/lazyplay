fe04cfb0858b38fd9e41534e73c640f3adfb0df0
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-07-01 20:14:27 +0700

    style: responsive card grid small device

--- src/pages/QueuePage.vue
+++ src/pages/QueuePage.vue
@@ -2,7 +2,7 @@
     <Container>
         <div class="">
             <SongInput @song-added="fetchSongs" />
-            <div class="mt-8 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-4">
+            <div class="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
                 <SongCard :songs="songs" @delete-song="deleteSong" />
             </div>
         </div>
