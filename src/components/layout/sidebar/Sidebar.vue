ea9ef4bee463573bfed3869ba893bccf32cf7550
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-07-02 14:25:45 +0700

    style: redesign sidebar

--- src/components/layout/sidebar/Sidebar.vue
+++ src/components/layout/sidebar/Sidebar.vue
@@ -5,14 +5,17 @@
                 <router-link to="/" class="w-fit mt-2">
                     <img src="/logo.png" width="56" alt="Logo" />
                 </router-link>
-                <div class="flex gap-3 flex-col mt-6">
-                    <ThemeToggle />
-                    <ProfileLink/>
-                    <Signout />
-                </div>
+            </div>
+            <div class="ml-6 mt-8 text-left">
+                <h1 class="text-2xl text-text font-medium mb-1">Lazyplay</h1>
+                <h3 class="text-sm flex items-center gap-2"><i class="text-primary fad fa-fire"></i>
+                    Welcome, {{ firstName }}
+                </h3>
+                <p class="text-sm flex items-center gap-2"><i class="text-primary fad fa-mug-hot"></i> {{ greetingMessage }}
+                </p>
             </div>
             <nav class="nav">
-                <div class="nav__menu p-6 bg-background rounded-l-none rounded-2xl">
+                <div class="nav__menu p-6 bg-background rounded-l-none rounded-2xl border border-stroke border-l-0">
                     <ul class="flex flex-col items-center gap-y-6">
                         <li v-for="item in menuItems" :key="item.label" class="nav__item w-full group">
                             <router-link :to="item.href"
@@ -30,11 +33,10 @@
             </nav>
         </div>
 
-        <div class="nav__footer text-left pl-6">
-            <span class="text-subtext text-sm transform rotate-180">&copy; {{ new Date().getFullYear() }} Dwi Wijaya</span>
-        </div>
-        <div @click="toggleSidebar" class="toggle lg:-left-64 left-5 sidebar__toggle" :class="{ '!left-[17rem]': toggle }">
-            <i class="fa-duotone fa-bars text-primary"></i>
+        <div class="flex gap-3 flex-col mt-6 ml-6">
+            <ThemeToggle />
+            <ProfileLink />
+            <Signout />
         </div>
     </aside>
 </template>
@@ -47,6 +49,7 @@ import { useRouter } from 'vue-router';
 import ThemeToggle from './ThemeToggle.vue';
 import Signout from './Signout.vue';
 import ProfileLink from './ProfileLink.vue';
+import { useUserStore } from '@stores/user';
 
 export default {
     // components: { ThemeToggle },
@@ -54,6 +57,9 @@ export default {
         return {
             sidebarElement: null,
             toggle: false,
+            currentTime: '',
+            greetingMessage: '',
+            firstName: '',
             menuItems: [
                 { label: 'Dashboard', href: '/', iconClass: 'fad fa-home' },
                 { label: 'Queue', href: '/queue', iconClass: 'fad fa-list-music' },
@@ -71,6 +77,7 @@ export default {
         },
     },
     mounted() {
+        this.userStore()
         this.sidebarElement = document.querySelector('.sidebar');
         document.addEventListener('mousedown', this.handleClickOutside);
         window.addEventListener('resize', this.handleResize);
@@ -81,7 +88,36 @@ export default {
         window.removeEventListener('resize', this.handleResize);
         document.body.style.overflow = '';
     },
+    created() {
+        this.updateTime();
+        this.updateGreeting();
+        setInterval(() => {
+            this.updateTime();
+            this.updateGreeting();
+        }, 60000); // Update setiap menit
+    },
+
     methods: {
+        async userStore() {
+            const userStore = useUserStore();
+            await userStore.fetchUser();
+            this.firstName = userStore.user.user_metadata.full_name;
+        },
+        updateTime() {
+            const now = new Date();
+            this.currentTime = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        },
+        updateGreeting() {
+            const now = new Date();
+            const hour = now.getHours();
+            if (hour < 12) {
+                this.greetingMessage = 'Good morning!';
+            } else if (hour < 18) {
+                this.greetingMessage = 'Good afternoon!';
+            } else {
+                this.greetingMessage = 'Good evening!';
+            }
+        },
         isActiveRoute(href) {
             return this.$route.path === href;
         },
@@ -112,6 +148,6 @@ export default {
             this.toggle = !this.toggle;
         },
     },
-    components: { ThemeToggle, Signout,ProfileLink }
+    components: { ThemeToggle, Signout, ProfileLink }
 };
 </script>
