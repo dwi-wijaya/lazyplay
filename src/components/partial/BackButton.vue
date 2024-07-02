76efb73053b3c0a4821666e682725ed9ea56870a
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-07-02 22:39:10 +0700

    style: fix back button on large device

--- src/components/partial/BackButton.vue
+++ src/components/partial/BackButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <router-link class="toggle sm:left-[70px] group-[.sidebar-expanded]/main:-left-[200px] hover:text-primary"><i class="fa-duotone fa-arrow-left-from-arc"></i>Back</router-link>
+    <router-link class="toggle left-[70px] lg:left-[17.5rem] group-[.sidebar-expanded]/main:-left-[200px] hover:text-primary"><i class="fa-duotone fa-arrow-left-from-arc"></i>Back</router-link>
 </template>
 
 <script setup>
