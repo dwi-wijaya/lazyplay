22c53f9e342e238d9a18bcb342e1077a02a9c75b
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-07-23 13:19:42 +0700

    feat: auth login with facebook

--- src/components/views/auth/AuthCard.vue
+++ src/components/views/auth/AuthCard.vue
@@ -7,15 +7,19 @@
                 <p class="text-subtext">Please enter your details to sign-in.</p>
             </div>
             <div class="flex gap-2 justify-center mt-4">
-                <button class="!border-neutral-300 card group hover:!bg-neutral-100 !bg-neutral-50 flex justify-center items-center w-28"
+                <button class="!border-neutral-300 card group hover:!bg-neutral-100 !bg-neutral-50 flex justify-center items-center w-full"
                     @click="signInWithProvider('github')">
                     <img class="w-6 group-hover:-rotate-12 duration-300 transition-all" src="@assets/logos/github.svg" alt="GitHub">
                 </button>
-                <button class="!border-neutral-300 card group group/card hover:!bg-neutral-100 flex !bg-neutral-50 justify-center items-center w-28"
+                <button class="!border-neutral-300 card group group/card hover:!bg-neutral-100 flex !bg-neutral-50 justify-center items-center w-full"
                     @click="signInWithProvider('google')">
                     <img class="w-6 group-hover:-rotate-12 duration-300 transition-all" src="@assets/logos/google.svg" alt="Google">
                 </button>
-                <button class="!border-neutral-300 card group group/card hover:!bg-neutral-100 flex !bg-neutral-50 justify-center items-center w-28"
+                <button class="!border-neutral-300 card group group/card hover:!bg-neutral-100 flex !bg-neutral-50 justify-center items-center w-full"
+                    @click="signInWithProvider('facebook')">
+                    <img class="w-6 group-hover:-rotate-12 duration-300 transition-all" src="@assets/logos/facebook.svg" alt="Twitter">
+                </button>
+                <button class="!border-neutral-300 card group group/card hover:!bg-neutral-100 flex !bg-neutral-50 justify-center items-center w-full"
                     @click="signInWithProvider('twitter')">
                     <img class="w-6 group-hover:-rotate-12 duration-300 transition-all" src="@assets/logos/twitter.svg" alt="Twitter">
                 </button>
