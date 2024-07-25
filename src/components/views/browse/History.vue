7d6e6ad2eed9bc259f73e8cbbf13223bf170a96d
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-07-25 20:32:58 +0700

    style: browse history card

--- src/components/views/browse/History.vue
+++ src/components/views/browse/History.vue
@@ -28,37 +28,40 @@
                 :key="index">
                 <div class="duration-500 relative h-full rounded-xl overflow-hidden">
                     <img :src="video.thumbnail" :alt="video.title"
-                        class="object-none w-full h-full transform transition-transform duration-300 group-hover:scale-[1.275] group-hover:blur-sm" />
-                    <div class="absolute z-10 top-0 left-0 flex flex-col gap-2 justify-center items-center w-full h-full">
+                        class="object-none w-full h-full transform transition-transform duration-300 group-hover:scale-[1.15] group-hover:blur-sm" />
+                    <div
+                        class="absolute z-10 top-0 left-0 flex flex-col gap-2 justify-center items-center w-full h-[calc(100%-3.75rem)]">
                         <button @click="AddToQueue(video)" :disabled="isCooldown || isDisable"
-                            class="!w-32 justify-center btn z-10 opacity-0 group-hover:opacity-100 disabled:cursor-not-allowed">
+                            class="!w-36 justify-center btn z-10 opacity-0 group-hover:opacity-100 disabled:cursor-not-allowed">
                             <i class="fad fa-signal-stream"></i>
                             {{
                                 isCooldown
-                                ? `Cooldown (${cooldownTime}s)`
-                                : 'Add To Queue'
+                                ? `(${cooldownTime}s)`
+                                : 'Request'
                             }}
                         </button>
                         <button @click="copyToClipboard(video.id, index)" :disabled="isCooldown"
-                            class="!w-32 justify-center btn z-10 opacity-0 group-hover:opacity-100">
+                            class="!w-36 justify-center btn z-10 opacity-0 group-hover:opacity-100">
                             <i :class="state.clipboardIcon[index] ?? 'fad fa-clipboard'"></i>
-                            {{ state.clipboardText[index] ?? 'Copy to clipboard' }}
+                            {{ state.clipboardText[index] ?? 'Copy' }}
                         </button>
                         <div
                             class="absolute top-0 left-0 bg-black opacity-0 group-hover:opacity-50 base-transition w-full h-full">
                         </div>
                     </div>
                     <div
-                        class="absolute inset-0 bg-gradient-to-b from-black/30 to-black opacity-90 transition-opacity duration-300">
+                        class="absolute inset-0 bg-gradient-to-b from-black/0 to-black opacity-90 transition-opacity duration-300">
                     </div>
                 </div>
-                <div class="absolute flex flex-col justify-end p-5 space-y-4 h-full w-full group-hover:hidden">
-                    <div class="flex flex-col justify-end">
-                        <div class="flex flex-col space-y-3 text-neutral-300">
+                <div class="absolute px-4 py-3 space-y-4 w-full bottom-0 h-15 backdrop-blur-sm rounded-b-2xl">
+                    <div class="flex gap-2">
+                        <img :src="video.artist_image" class="w-8 h-8 rounded-md" alt="" srcset="">
+                        <div class="flex flex-col">
                             <h3
-                                class="line-clamp-1 leading-5 !mt-1 font-medium text-neutral-200 group-hover:underline group-hover:underline-offset-4 ">
+                                class="line-clamp-1 leading-5 font-medium text-neutral-200 ">
                                 {{ video.title }}
                             </h3>
+                            <p class="text-xs line-clamp-1 m-0 text-neutral-400">{{ video.artist }}</p>
                         </div>
                     </div>
                 </div>
@@ -115,7 +118,7 @@ export default {
     data() {
         return {
             state: {
-                clipboardText: this.userHistory.map(() => 'Copy to clipboard'),
+                clipboardText: this.userHistory.map(() => 'Copy'),
                 clipboardIcon: this.userHistory.map(() => 'fad fa-clipboard')
             }
         };
@@ -149,10 +152,10 @@ export default {
             await copy(`https://music.youtube.com/watch?v=${videoId}`);
 
             if (copied.value) {
-                this.state.clipboardText[index] = 'Copied!';
+                this.state.clipboardText[index] = 'Copied';
                 this.state.clipboardIcon[index] = 'fad fa-check';
                 setTimeout(() => {
-                    this.state.clipboardText[index] = 'Copy to clipboard';
+                    this.state.clipboardText[index] = 'Copy';
                     this.state.clipboardIcon[index] = 'fad fa-clipboard';
                 }, 3000);
             }
